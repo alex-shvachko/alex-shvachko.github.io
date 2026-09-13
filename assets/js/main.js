99a084c80da77785d2f0839b5de4671b821b8fea
Tichzi <34437458+alex-shvachko@users.noreply.github.com>
2026-09-13 20:07:16 +0300

    Render local Three.js hero scene

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js';
+import * as THREE from './vendor/three.module.js';
 
 const canvas = document.querySelector('#scene');
 const scene = new THREE.Scene();
